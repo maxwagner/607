4d164e2da53ecfb869ecb267825e6d5aadcce82f
maxwagner <maxwell.v.wagner@gmail.com>
2015-09-26 14:09:33 -0400

    added everything else for p1

--- project01/project01.R
+++ project01/project01.R
@@ -5,22 +5,25 @@ chess_sep <- unlist(str_extract_all(chess, "\\d+\\s[|]\\s(\\w+[ -]+)+[|]\\d[.]\\
 
 id <- unlist(str_extract(chess_sep, "\\d+"))
 name <- str_trim(unlist(str_extract(chess_sep, "\\s(\\w+[ -]+)+")))
-state <- str_trim(unlist(str_extract(chess_sep, "\\s\\w{2}\\s")));state
+state <- unlist(str_extract(chess_sep, "[|]\\s\\w{2}\\s[|]"))
+state <- str_trim(str_sub(state, 2, -2));
 points <- unlist(str_extract(chess_sep, "\\d[.]\\d"))
-
 starting_score <- unlist(str_extract(chess_sep, "R:\\s\\d+"))
-starting_score <- substring(starting_score, 4);starting_score
-
+starting_score <- substring(starting_score, 4);
 opponents <- unlist(str_extract(chess_sep, "[|](\\w\\s+\\d*[|]){7}"))
 opponents <- unlist(str_replace_all(opponents, "[:alpha:]+", ""))
 opponents <- unlist(str_replace_all(opponents, "[:space:]+", ""))
-opponents <- str_sub(opponents, 2, -2);opponents
+opponents <- str_sub(opponents, 2, -2)
 opponents <- unlist(str_split(opponents, "[|]"))
-opponents <- matrix(opponents, nrow = 64, byrow = T)
-colnames(opponents) <- paste('o', 1:7, sep = "")
-# opponents <- unlist(str_extract(opponents, "\\d+"));opponents
+opponents <- unlist(str_extract(opponents, "\\d+"))
 
-prelim <- cbind(id, name, state, points, starting_score, opponents);prelim
+for (i in 1:length(opponents)) {
+  if (!is.na(opponents[i])) {
+    opponents[i] <- starting_score[as.numeric(opponents[i])]
+  }
+}
 
-
-write.csv(prelim, file = "chess_output.csv")
+opponents <- data.frame(matrix(as.numeric(opponents), nrow = 64, byrow = T))
+opponents <- round(apply(opponents, 1, mean, na.rm = T), 2)
+chess_final <- cbind(name, state, points, starting_score, opponents);chess_final
+write.csv(chess_final, file = "chess_output.csv")
