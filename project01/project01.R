2c311c0c0bec3181e2e5bbdd5e56ab65565150ab
maxwagner <maxwell.v.wagner@gmail.com>
2015-09-26 06:38:39 -0400

    added starting / ending scores

--- project01/project01.R
+++ project01/project01.R
@@ -1,10 +1,18 @@
 library(stringr)
-chess <- scan(file = "tournamentinfo.txt", what = "character", skip = "3")
+chess <- scan(file = "https://raw.githubusercontent.com/maxwagner/607/master/project01/tournamentinfo.txt", what = "character", skip = "3")
 chess <- str_c(chess, collapse = " ")
-
-# sep the players out, really big and ugly
 chess_sep <- unlist(str_extract_all(chess, "\\d+\\s[|]\\s(\\w+[ -]+)+[|]\\d[.]\\d\\s+[|](\\w\\s+\\d*[|]){7}\\s\\w+\\s[|]\\s\\d+\\s[/]\\sR:\\s[:alnum:]+\\s*->\\s*[:alnum:]+\\s[|].+?[|](\\w*\\s[|]){7}"))
 
+
+
 id <- unlist(str_extract(chess_sep, "\\d+"))
+
 names <- str_trim(unlist(str_extract(chess_sep, "\\s(\\w+[ -]+)+")))
+
+starting_score <- unlist(str_extract(chess_sep, "R:\\s\\d+"))
+starting_score <- substring(starting_score, 4);starting_score
+
+ending_score <- unlist(str_extract(chess_sep, "->\\s*\\d+"))
+ending_score <- str_trim(substring(ending_score, 3));ending_score
+  
 points <- unlist(str_extract(chess_sep, "\\d[.]\\d"))
