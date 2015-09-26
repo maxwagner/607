8dd48779ac8f000e85c777f67a66da394ef8ac5c
maxwagner <maxwell.v.wagner@gmail.com>
2015-09-26 11:58:53 -0400

    pulled oppon info

--- project01/project01.R
+++ project01/project01.R
@@ -3,16 +3,24 @@ chess <- scan(file = "https://raw.githubusercontent.com/maxwagner/607/master/pro
 chess <- str_c(chess, collapse = " ")
 chess_sep <- unlist(str_extract_all(chess, "\\d+\\s[|]\\s(\\w+[ -]+)+[|]\\d[.]\\d\\s+[|](\\w\\s+\\d*[|]){7}\\s\\w+\\s[|]\\s\\d+\\s[/]\\sR:\\s[:alnum:]+\\s*->\\s*[:alnum:]+\\s[|].+?[|](\\w*\\s[|]){7}"))
 
-
-
 id <- unlist(str_extract(chess_sep, "\\d+"))
-
-names <- str_trim(unlist(str_extract(chess_sep, "\\s(\\w+[ -]+)+")))
+name <- str_trim(unlist(str_extract(chess_sep, "\\s(\\w+[ -]+)+")))
+state <- str_trim(unlist(str_extract(chess_sep, "\\s\\w{2}\\s")));state
+points <- unlist(str_extract(chess_sep, "\\d[.]\\d"))
 
 starting_score <- unlist(str_extract(chess_sep, "R:\\s\\d+"))
 starting_score <- substring(starting_score, 4);starting_score
 
-ending_score <- unlist(str_extract(chess_sep, "->\\s*\\d+"))
-ending_score <- str_trim(substring(ending_score, 3));ending_score
-  
-points <- unlist(str_extract(chess_sep, "\\d[.]\\d"))
+opponents <- unlist(str_extract(chess_sep, "[|](\\w\\s+\\d*[|]){7}"))
+opponents <- unlist(str_replace_all(opponents, "[:alpha:]+", ""))
+opponents <- unlist(str_replace_all(opponents, "[:space:]+", ""))
+opponents <- str_sub(opponents, 2, -2);opponents
+opponents <- unlist(str_split(opponents, "[|]"))
+opponents <- matrix(opponents, nrow = 64, byrow = T)
+colnames(opponents) <- paste('o', 1:7, sep = "")
+# opponents <- unlist(str_extract(opponents, "\\d+"));opponents
+
+prelim <- cbind(id, name, state, points, starting_score, opponents);prelim
+
+
+write.csv(prelim, file = "chess_output.csv")
