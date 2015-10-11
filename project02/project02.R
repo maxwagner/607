b3d9ecbda9148739e0c21c9a7c0f76ed31c11140
max <maxwell.v.wagner@gmail.com>
2015-10-11 02:03:12 -0400

    more of 2/3

--- project02/project02.R
+++ project02/project02.R
@@ -1,6 +1,15 @@
 library(rvest)
 library(stringr)
 library(dplyr)
+library(tidyr)
 
-# read in the mammals txt file
-mammals <- read.table("mammals.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+mammals <- read.table("https://raw.githubusercontent.com/maxwagner/607/master/project02/mammals.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+mammals <- tbl_df(mammals)
+colnames(mammals) <- tolower(colnames(mammals))
+colnames(mammals) <- str_replace(colnames(mammals), "msw05_", "")
+colnames(mammals) <- str_replace(colnames(mammals), "x[0-9._]+", "")
+mammals <- select(mammals, order, family, binomial, contains("bodymass"))
+mammals[mammals == -999] <- NA
+mammals <- unite(mammals, full_name, order, family, binomial, sep = ", ")
+mammals <- mammals[,1:4]
+mammals <- mammals[complete.cases(mammals),]
